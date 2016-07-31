74c591306f68ce9586c7eb4cb8ffb20abc8695ca
Yoshiki Aoki <yoshiki_aoki@dwango.co.jp>
2016-07-31 16:31:10 +0900

    Add external script installer

---
+++ test/aws-charge.test.js
@@ -0,0 +1,9 @@
+var assert = require('power-assert');
+var Strcolor = require('../scripts/aws-charge');
+
+describe('Sample test', function() {
+  it('Sample test', function(done) {
+    assert(1 === 1);
+    done();
+  });
+});
